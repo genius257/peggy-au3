64a8538b0410bc6380ef937975c5cc2372b28c29
Anders Pedersen <hot415@hotmail.com>
2025-01-26 00:01:39 +0100

    Added support for group ast peggy node

--- src/peggy-au3.ts
+++ src/peggy-au3.ts
@@ -279,6 +279,8 @@ class Peggyau3 implements IPeggyau3 {
                 return `${this.functionName("Parser_RuleRef")}, ${this.functionName("peg_f" + ast.name)}`;
             case "zero_or_more":
                 return `${this.functionName("Parser_ZeroOrMore")}, ${this.functionName("Array")}(${this.ast2code(ast.expression)})`;
+            case "group":
+                return `${this.functionName("Parser_Group")}, ${this.functionName("Array")}(${this.ast2code(ast.expression)})`;
             default:
                 throw new Error("unhandled type: " + ast.type);
         }
